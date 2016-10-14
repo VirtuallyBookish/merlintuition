271a276dbcec2327ed62f0c67ede38caf8db6e4a
thoma_000 <virtuallybookish@gmail.com>
2016-10-14 22:34:12 +0100

    Changed Title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Merlin Tuition</title>
 
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
