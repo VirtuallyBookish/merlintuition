992917d98d2c13b7136c352deeab063f9cf99fc3
thoma_000 <virtuallybookish@gmail.com>
2016-11-03 08:05:03 +0000

    Added jumbo cards to home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,32 @@
 
     @include('components.hero')
 
-    <div class="container container-home">
+    <div class="container-fluid container-home">
+        <div class="row">
+            <div class="col-md-4 jumbo-card">
+                <div id="jumbo-1" class="jumbo-content">
+                    <h1 class="text-center">Test Jumbo Card</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+
+            <div id="jumbo-2" class="col-md-4 jumbo-card">
+                <div class="jumbo-content">
+                    <h1 class="text-center">Test Jumbo Card</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+
+            <div id="jumbo-3" class="col-md-4 jumbo-card">
+                <div class="jumbo-content">
+                    <h1 class="text-center">Test Jumbo Card</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="container container-home">
 
         <div class="row sales-row">
             <div class="col-sm-3">
@@ -70,5 +95,5 @@
             </div>
         </div>
 
-    </div>
+    </div> --}}
 @endsection
