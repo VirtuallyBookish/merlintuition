c59d532c2455392b1fd67ccc65ec157084795b08
thoma_000 <virtuallybookish@gmail.com>
2016-11-03 07:42:39 +0000

    changed look of hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,56 +1,32 @@
 <div class="container-fluid main-header hero">
-    {{-- <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><img class="logo" src="/images/circle_hat.png">Merlin Tuition</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Find a Tutor</a></li>
-                    <li><a href="#about">Become a Tutor</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div> --}}
     </nav>
-
     <div class="container text-center">
-        {{-- <div class="row">
-            <div class="col-sm-12 hero-navigation">
-                <p>Find a Tutor | Become a Tutor | About | Contact</p>
-            </div>
-        </div> --}}
-
         <div class="row">
             <div class="hero-content">
                     <h1>Brighton's Bespoke Tuition Service</h1>
                     <h2>Call To Action - CHANGE</h2>
             </div>
         </div>
-
         <div class="row">
             <div class="hero-form">
                 <div class="row">
                     <form>
                         <div class="col-sm-4">
-                            <select class="form-control">
-                                <option>English</option>
-                            </select>
+                            <div class="hero-select">
+                                <select class="form-control">
+                                    <option>English</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="col-sm-4">
-                            <select class="form-control">
-                                <option>Primary</option>
-                            </select>
+                            <div class="hero-select">
+                                <select class="form-control">
+                                    <option>Primary</option>
+                                </select>
+                            </div>
                         </div>
                         <div class="col-sm-4">
-                            <button class="form-control">Find a tutor</button>
+                            <button class="form-control main-cta">Find a tutor</button>
                         </div>
                     </form>
                 </div>
